cd4da8c406e69d7aec3533165c45799147ba97cc
adammuhd15 <adam_muhd96@hotmail.co.uk>
2023-03-04 17:26:09 +0800

    Added docstrings and API related files

---
+++ CharacterSearch/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  CharacterSearch
+//
+//  Created by Adam Muhammad on 04/03/2023.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
