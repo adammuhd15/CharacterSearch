6b245ac7b1873d10b9aa6e93bcb890cd0d5672bb
adammuhd15 <adam_muhd96@hotmail.co.uk>
2023-06-09 12:12:50 +0800

    Set up view of Character screen

---
+++ CharacterSearch/Resources/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  CharacterSearch
+//
+//  Created by Adam Muhammad on 09/06/2023.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach({
+            addSubview($0)
+        })
+    }
+}
