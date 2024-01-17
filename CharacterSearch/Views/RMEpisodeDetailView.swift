caed06da8089d11ac6e6caa50eda390225c5fe95
adammuhd15 <adam_muhd96@hotmail.co.uk>
2024-01-18 00:02:16 +0800

    Add API Cache Layer

---
+++ CharacterSearch/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  RMEpisodeDetailView.swift
+//  CharacterSearch
+//
+//  Created by Adam Muhammad on 17/01/2024.
+//
+
+import UIKit
+
+final class RMEpisodeDetailView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+
+}
