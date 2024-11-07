939c0f953185f88901db793bba4575a69ba0f272
adammuhd15 <adam_muhd96@hotmail.co.uk>
2024-11-07 19:29:56 +0800

    Add Location List View

---
+++ CharacterSearch/Models/APIResponseTypes/RMGetLocationsResponse.swift
@@ -0,0 +1,20 @@
+//
+//  RMGetLocationsResponse.swift
+//  CharacterSearch
+//
+//  Created by Adam Muhammad on 07/11/2024.
+//
+
+import Foundation
+
+struct RMGetAllLocationsResponse: Codable {
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
+    }
+    
+    let info: Info
+    let results: [RMLocation]
+}
