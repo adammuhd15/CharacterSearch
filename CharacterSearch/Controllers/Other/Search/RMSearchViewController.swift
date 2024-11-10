bb9a2d58d1c746028c71458633001fc202165067
adammuhd15 <adam_muhd96@hotmail.co.uk>
2024-11-10 23:07:28 +0800

    Add Search Option Bottom Sheet

--- CharacterSearch/Controllers/Other/Search/RMSearchViewController.swift
+++ CharacterSearch/Controllers/Other/Search/RMSearchViewController.swift
@@ -93,6 +93,11 @@ final class RMSearchViewController: UIViewController {
 
 extension RMSearchViewController: RMSearchViewDelegate {
     func rmSearchView(with searchView: RMSearchView, didSelectOption option: RMSearchInputViewViewModel.DynamicOption) {
-        print("Should present option picker")
+        let vc = RMSearchOptionPickerViewController(option: option) { selection in
+            print("Did select \(selection)")
+        }
+        vc.sheetPresentationController?.detents = [.medium()]
+        vc.sheetPresentationController?.prefersGrabberVisible = true
+        present(vc, animated: true)
     }
 }
